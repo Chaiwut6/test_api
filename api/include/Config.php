7977bf44545841b319cf13196d39c50b75bc9de7
Chaiwut <chivwut0646@gmail.com>
2024-10-09 16:08:28 +0700

    delete index file

--- api/include/Config.php
+++ api/include/Config.php
@@ -3,4 +3,8 @@
  define('DB_PASSWORD', "");
  define('DB_HOST', 'localhost');
  define('DB_NAME', 'test_api');
+// define('DB_USERNAME', 'if0_37470021');
+// define('DB_PASSWORD', "pSgb2fyDavap7z");
+// define('DB_HOST', 'sql200.infinityfree.com');
+// define('DB_NAME', 'if0_37470021_test_api');
 ?>
